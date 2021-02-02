74c249f4c54e2e2e5556498fa2521d329ce697cd
Dong Zhang <dongzhanghz@gmail.com>
2021-02-02 17:03:17 -0500

    Add files via upload

---
+++ finding_lanes.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+
+image = cv2.imread('data/test_image.jpg')
+lane_image = np.copy(image)
+
+gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+
+
+
+cv2.imshow('result',gray)
+
+cv2.waitKey(0)
+
+
+
